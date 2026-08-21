243890f8ea7146accb39e3fc4bb6328e7e6845a4
Per Andreas Gulbrandsen <peran.gulbrandsen@zolve.no>
2026-08-21 12:34:44 +0200

    Exercise 1

--- app/src/main.c
+++ app/src/main.c
@@ -8,6 +8,53 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 #define PRIO_A 5
 #define PRIO_B 5
 
+#define LOW_PRIO 7
+#define MED_PRIO 5
+#define HIGH_PRIO 3
+#define COOP_PRIO (-1)
+
+void t_low_fn(void *p1, void *p2, void *p3)
+{
+    while (1) {
+        printk("T_LOW is running\n");
+        k_msleep(300);
+    }
+}
+void t_med_fn(void *p1, void *p2, void *p3)
+{
+    while (1) {
+        printk("T_MED is running\n");
+        k_msleep(200);
+    }
+}
+void t_high_fn(void *p1, void *p2, void *p3)
+{
+    while (1) {
+        printk("T_HIGH is running\n");
+        k_msleep(100);
+    }
+}
+
+void t_coop_fn(void *p1, void *p2, void *p3)
+{
+    for (int i = 0; i < 5; i++) {
+        printk("T_COOP is running (%d)\n", i);
+        k_busy_wait(100000);
+    }
+    k_yield();
+    printk("T_COOP is exiting\n");
+}
+
+K_THREAD_DEFINE(t_low, STACK_SIZE, t_low_fn,
+                NULL, NULL, NULL, LOW_PRIO, 0, 0);
+K_THREAD_DEFINE(t_med, STACK_SIZE, t_med_fn,
+                NULL, NULL, NULL, MED_PRIO, 0, 0);
+K_THREAD_DEFINE(t_high, STACK_SIZE, t_high_fn,
+                NULL, NULL, NULL, HIGH_PRIO, 0, 0);
+K_THREAD_DEFINE(t_coop, STACK_SIZE, t_coop_fn,
+                NULL, NULL, NULL, COOP_PRIO, 0, 0);
+
+#if 0
 void thread_a_fn(void *p1, void *p2, void *p3)
 {
     while (1) {
@@ -26,6 +73,7 @@ K_THREAD_DEFINE(thread_a, STACK_SIZE, thread_a_fn,
                 NULL, NULL, NULL, PRIO_A, 0, 0);
 K_THREAD_DEFINE(thread_b, STACK_SIZE, thread_b_fn,
                 NULL, NULL, NULL, PRIO_B, 0, 0);
+#endif
 
 int main(void)
 {
